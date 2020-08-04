979bf6c99b1e21018990ec84a4d6b730f4d2a5da
kommalyskb <kommalyskb@users.noreply.github.com>
2020-08-04 23:36:14 +0700

    Add project files.

---
+++ Day1/Client/Helpers/Utitlities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day1.Client.Helpers
+{
+    public static class Utitlities
+    {
+        public static string CustomToUpper(string value)
+        {
+            return value.ToUpper();
+        }
+    }
+}
